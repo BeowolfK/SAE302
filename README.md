b5666098b36cbd2b5fc15f00b5869c2ab96ddc05
Kénan Meylan <kenan.meylan@gmail.com>
2022-12-13 12:15:33 +0100

    docker, flask app, databse and php my admin

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # SAE302
 SAE 302 - Developpement d'applications communicante
+
+## Représentation de la base de données
+
+![Représentation](./image//mcd.jpg)
+
+## Utilisation de Docker
+
+[Télécharger Docker](https://docs.docker.com/desktop/) selon le système d'exploitation
+Pour lancer les containers, la commande est la suivante :
+
+```bash
+docker-compose up
+```
+
+Nous pouvons acceder a PHPMyAdmin sur l'URL `http://localhost:8080`.
+Pour acceder a la base de donnée, nous pouvons nous connecter sur `localhost:3307`
+Nous pouvons aussi acceder a la base de données directement sur le container grace a la commande
+
+```bash
+docker exec -it <CONTAINER-ID> bash
+```
+
+Ici, `CONTAINER-ID = sae302-db-1`.
+
+Grace aux volumes persistants, nos bases de données sont dans le dossier `./database`
+
+## Serveur Web 
+
+Pour installer les paquets nécessaire, nous pouvons faire la commande suivante:
+
+```bash
+python -m pip install -r requirements.txt
+```
+
+La page web est ensuite visible, une fois le programme lancé, sur l'ip de la machine (Port 80 par défaut).
+
+Sur serveur Linux, pour lancer le site web en production, il faut utiliser la commande :
+
+```bash
+gunicorn -w -4 --reload -b localhost:80 "web.flask-app:home()"
+```
---
+++ web/flask-app.py
@@ -0,0 +1,58 @@
+import mysql.connector
+from flask import Flask, render_template, redirect
+import sys
+
+"""
+Need to improve database select 
+website is not dynamic
+"""
+
+app = Flask(__name__)
+
+try:
+    con = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        port="3307",
+        password="root",
+        database="universite",
+    )
+except mysql.connector.Error as e:
+    print("Exception : ", e)
+    sys.exit(1)
+else:
+    print("Connexion réussie")
+
+cur = con.cursor()
+
+
+@app.route("/")
+def home():
+    cur.execute("SELECT etudiant.id_etudiant, nom, prenom, sexe, annee, \
+                AVG(note) FROM etudiant INNER JOIN note ON \
+                etudiant.id_etudiant = note.id_etudiant \
+                GROUP BY etudiant.id_etudiant")
+    result = cur.fetchall()
+    return render_template('home.html', result=result)
+
+
+@app.route("/<etudiant>")
+def note_student(etudiant):
+    try:
+        cur.execute(f"SELECT matiere.nom, AVG(NOTE) FROM note INNER JOIN matiere ON note.id_matiere = matiere.id_matiere WHERE id_etudiant = {etudiant} GROUP BY note.id_matiere;")
+        result = cur.fetchall()
+        if result == []:
+            return redirect('/404.html')
+        return render_template('etudiant.html', result=result)
+    except mysql.connector.errors.ProgrammingError:
+        return redirect('/404')
+
+
+@app.route("/error404")
+def page404():
+    return render_template('404.html')
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=80)

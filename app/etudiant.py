5382fef4d8913bc1f278191edb8cf9b470164022
laborifermc <laborifermc@gmail.com>
2022-12-14 10:04:29 +0100

    etudiant, personne
    GET et set de etudiant et personne

---
+++ app/etudiant.py
@@ -0,0 +1,37 @@
+from personne import Personne
+
+class Etudiant(Personne):
+    def __init__(self, *args):
+        Personne.__init__(self,*args)
+        
+        if type(int(args[3])) == int :
+
+          
+            self.__annee = int(args[3])
+        else : 
+            self.__annee = 0    
+            
+        if len(args) > 4 :
+            self.__matiere = args[4]
+        else :
+            self.__matiere = ""
+        
+        if len(args) > 5 :
+            self.__moyenne = args[5]
+        else :
+            self.__moyenne = 0
+                
+    def set_annee(self, x):
+        self.__annee = x 
+    def get_annee(self):
+        return self.__annee
+    
+    def set_matiere(self, x):
+        self.__matiere = x 
+    def get_matiere(self):
+        return self.__matiere
+
+    def set_moyenne(self, x):
+        self.__moyenne = x 
+    def get_moyenne(self):
+        return self.__moyenne
---
+++ app/personne.py
@@ -0,0 +1,35 @@
+class Personne: 
+    def __init__(self, *args):
+        if len(args) > 0 :
+            print(args[0])
+            self.__prenom = args[0]
+        else :
+            self.__prenom = ""
+            
+        if len(args) > 1 :
+            self.__nom = args[1]
+        else:
+            self.__nom = ""
+
+        if len(args) > 2 :
+            self.__email = args[2]
+        else:
+            self.__email = ""
+    
+    def set_prenom(self, x):
+        self.__prenom = x 
+    def get_prenom(self):
+        return self.__prenom
+    
+    def set_nom(self, x):
+        self.__nom = x 
+    def get_nom(self):
+        return self.__nom
+    
+    def set_email(self, x):
+        self.__email = x 
+    def     get_email(self):
+        return self.__email
+    
+    def afficher(self):
+        return f"{self.__prenom} {self.__nom} {self.__email}"

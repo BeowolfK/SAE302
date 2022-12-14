dab76333f56955fcce245a832fa036f98c4344dd
laborifermc <laborifermc@gmail.com>
2022-12-14 15:11:08 +0100

    Mise en forme de la page Etudiant
    Menu  "fait, avec access à l'espace étudiant

--- app/application.py
+++ app/application.py
@@ -2,12 +2,34 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+
+class FirstWindow(Screen):
+    pass
+class StudWindow(Screen): 
+    pass 
+class TeachWindow(Screen):
+    pass
+class WindowManager(ScreenManager):
+    pass
 Builder.load_file("application.kv")
 
 class Application(App): 
     def build(self):
         pass
 
+    def valider(self): 
+        
+
+        
+        if self.root.get_screen("hub").ids.identif.text != "" and self.root.get_screen("hub").ids.mdp.text != "":
+            identifiant = self.root.get_screen("hub").ids.identif.text
+            motdp = self.root.get_screen("hub").ids.mdp.text 
+           
+            return "second"
+       
 if __name__ == "__main__":
     app = Application()
     app.run()  

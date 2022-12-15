9f505732427e0dbb6c6f268123178faa8dfa5cd5
laborifermc <laborifermc@gmail.com>
2022-12-15 08:21:34 +0100

    Avancement

--- app/application.py
+++ app/application.py
@@ -1,11 +1,12 @@
 import kivy 
+from login import * 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-
+# CREATION DES CLASS REPRESANTANT NOS WINDOWS
 class FirstWindow(Screen):
     pass
 class StudWindow(Screen): 
@@ -21,14 +22,18 @@ class Application(App):
         pass
 
     def valider(self): 
-        
-
-        
+        """
+        Fait une requete pour savoir si c'est un étudiant, un prof, autre et en fonction renvoie la valeur pour switch faire la bonne fenêtre. 
+        """
         if self.root.get_screen("hub").ids.identif.text != "" and self.root.get_screen("hub").ids.mdp.text != "":
             identifiant = self.root.get_screen("hub").ids.identif.text
             motdp = self.root.get_screen("hub").ids.mdp.text 
-           
-            return "second"
+            verif = verify(identifiant, motdp)
+            if verif != None : 
+                if verif[0] == 'etu':
+                    self.root.get_screen("second").ids.nom.text = str(verif[1]) 
+                    return "second"
+
        
 if __name__ == "__main__":
     app = Application()

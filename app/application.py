9ee32ce484c4054dfbc853aa2682e9935bb46422
Kénan Meylan <kenan.meylan@gmail.com>
2023-01-08 11:56:35 +0100

    new etudiant verification and async image in admin panel

--- app/application.py
+++ app/application.py
@@ -1,8 +1,11 @@
 from login import verify
-from management import *
+# from management import *
+from management import liste_etu, info_etu, info_prof, change_status, \
+    new_etudiant, panel_note
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.image import AsyncImage
 from kivy.uix.screenmanager import ScreenManager, Screen
 from etudiant import Etudiant
 from kivy.uix.scrollview import ScrollView
@@ -114,23 +117,29 @@ class Application(App):
                     self.get_stats()
                     return "second"
                 if verif[0] == "prof":
-                    id = verif [1]
+                    id = verif[1]
                     info = info_prof(id)
                     self.resetchamp(True)
-                    return("teacher")
-                    
+                    return ("teacher")
+
                 if verif[0] == "admin":
                     return "admin"
             else:
                 self.resetchamp(False)
-            
-    
+
     def get_stud(self):
         liste = liste_etu()
         grid = self.root.get_screen("liste_etu").ids.grid_etu
-        
+        # image path : http://54.37.226.86:8000/<id>-<NOM>-<PRENOM>.png
         for etu in liste:
-            grid.add_widget(self.create_lbl("photo"))
+            pp = AsyncImage(
+                    source="http://54.37.226.86:8000/{}-{}-{}.png".format(
+                        etu[7],
+                        etu[1].upper(),
+                        etu[2].upper()
+                    )
+                )
+            grid.add_widget(pp)
             grid.add_widget(self.create_lbl(etu[0]))
             grid.add_widget(self.create_lbl(etu[1]))
             grid.add_widget(self.create_lbl(etu[2]))
@@ -184,19 +193,62 @@ class Application(App):
             print(e)
 
     def add_student(self):
+        screen = self.root.get_screen("addstudent")
+        label_nom = screen.ids.l_nom
+        label_prenom = screen.ids.l_prenom
+        label_sexe = screen.ids.l_sexe
+        label_annee = screen.ids.l_annee
+        label_mdp = screen.ids.l_mdp
+        label_button = screen.ids.b_filechose
+        lbls = [
+            label_nom,
+            label_prenom,
+            label_sexe,
+            label_annee,
+            label_mdp,
+            label_button]
+        for el in lbls:
+            el.color = (192, 192, 192, 1)
         try:
-            filename = self.root.get_screen("addstudent").ids.i_etu.source
-            nom = self.root.get_screen("addstudent").ids.t_nom.text
-            prenom = self.root.get_screen("addstudent").ids.t_prenom.text
-            sexe = "M" if self.root.get_screen("addstudent").ids.ck_h.active \
+            filename = screen.ids.i_etu.source
+            nom = screen.ids.t_nom.text
+            prenom = screen.ids.t_prenom.text
+            sexe = "M" if screen.ids.ck_h.active \
                 else "F"
-            annee = 1 if self.root.get_screen("addstudent").ids.ck_1a.active \
+            if not screen.ids.ck_h.active and not screen.ids.ck_h.active:
+                sexe = ""
+            annee = 1 if screen.ids.ck_1a.active \
                 else 2
-            mdp = self.root.get_screen("addstudent").ids.t_mdp.text
-            res = new_etudiant(nom, prenom, annee, sexe, filename, mdp)
-            print(res)
-            if not res:
-                print("Error")
+            if not screen.ids.ck_1a.active and not screen.ids.ck_2a.active:
+                annee = ""
+            mdp = screen.ids.t_mdp.text
+            flag = True
+            if filename == "":
+                flag = False
+                label_button.color = (1, 0, 0, .4)
+            if nom == "":
+                flag = False
+                label_nom.color = (1, 0, 0, .4)
+            if prenom == "":
+                flag = False
+                label_prenom.color = (1, 0, 0, .4)
+            if sexe not in ["M", "F"]:
+                flag = False
+                label_sexe.color = (1, 0, 0, .4)
+            if annee not in [1, 2]:
+                flag = False
+                label_annee.color = (1, 0, 0, .4)
+            if mdp == "":
+                flag = False
+                label_mdp.color = (1, 0, 0, .4)
+            if flag:
+                self.reset_addstudent()
+                res = new_etudiant(nom, prenom, annee, sexe, filename, mdp)
+                print(res)
+                if not res:
+                    print("Error")
+            else:
+                print("Tous les champs doivent ")
 
         except Exception as e:
             print(e)
@@ -214,20 +266,21 @@ class Application(App):
 
     def get_stats(self):
         id = self.user.get_id()
-        
+
         note = panel_note(id)
         print(note)
         grid = self.root.get_screen("second").ids.grid1
-        for grade in note : 
+        for grade in note:
             grid.add_widget(self.create_lbl(grade[0].title()))
             grid.add_widget(self.create_lbl(",".join(grade[1])))
             grid.add_widget(self.create_lbl(grade[2]))
+
     def clear_note(self):
         self.root.get_screen("second").ids.grid1.clear_widgets()
 
-
-    def espace_note(self): 
+    def espace_note(self):
         self.root.add_widget(ScrollView(size_hint=(.3, 2)))
+
 
 if __name__ == "__main__":
     app = Application()

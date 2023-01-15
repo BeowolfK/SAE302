64c0ffa4a42b84cfc50debfb5739cb5a1fc88b89
BeowolfK0 <kenan.meylan@gmail.com>
2023-01-15 15:59:03 +0100

    panel vie scolaire bug in database

--- app/test2.py
+++ app/test2.py
@@ -1,7 +1,2 @@
-from management import *
-from etudiant import *
-from login import *
-from datetime import *
-
-
-add_abscence_vie_scolaire(3,1,"r309","2023-01-14","15","test")
+from datetime import datetime
+print(datetime.now().date().strftime('%d/%m/%Y'))
